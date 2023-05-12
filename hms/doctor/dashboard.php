6683e0a81e2f658c78eb7250ed40c0add0708fdd
sumankandel-developer <128758496+sumankandel-developer@users.noreply.github.com>
2023-05-13 00:33:20 +0100

    unwanted file

--- hms/doctor/dashboard.php
+++ hms/doctor/dashboard.php
@@ -105,10 +105,6 @@ if(strlen($_SESSION['id']==0)) {
 	<?php include('include/footer.php');?>
 			<!-- end: FOOTER -->
 		
-			<!-- start: SETTINGS -->
-	<?php include('include/setting.php');?>
-			
-			<!-- end: SETTINGS -->
 		</div>
 		<!-- start: MAIN JAVASCRIPTS -->
 		<script src="vendor/jquery/jquery.min.js"></script>
